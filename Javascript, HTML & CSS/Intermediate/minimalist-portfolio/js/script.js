2a313c746ea6af6dbc1f1a2e6a369059e1116c8f
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2021-06-30 10:42:06 -0300

    Homepage finished

--- Javascript, HTML & CSS/Intermediate/minimalist-portfolio/js/script.js
+++ Javascript, HTML & CSS/Intermediate/minimalist-portfolio/js/script.js
@@ -6,33 +6,53 @@
    
 
        /******************* Selectors *******************/
-const menuCheckbox = document.querySelector("#burger-cbx");
-const menuIcons = document.querySelector(".burger-icons");
+const menuCheckbox = document.querySelector("input#menu-checkbox");
+const menuIcons = document.querySelector("label.menu-icons");
 
         /******************* Event Listeners *******************/
-menuCheckbox.addEventListener('click', showMenu);
-// menuIcons.addEventListener('click', showMenu);
+menuCheckbox.addEventListener('change', toggleIcon);
+// menuIcons.addEventListener('click', toggleIcon);
 
         /******************* Functions *******************/
+
+function toggleIcon() {
+        const burgerIcon = document.querySelector('#burger-icon');
+        const closeIcon = document.querySelector('#close-icon');
+
+        // console.log('worked')
+
+        if (menuCheckbox.checked) {
+                burgerIcon.classList.add('element-none');
+                closeIcon.classList.remove('element-none');
+                // console.log('1-worked')
+        }
+        else {
+                burgerIcon.classList.remove('element-none');
+                closeIcon.classList.add('element-none');
+                // console.log('2-worked')
+        }
+}
 
 // Shows the mobile menu 
 // function showMenu() {
 //     const navList = document.querySelector("#nav-list");
 
+//     navList.addEventListener("transitionend", () => {
+//         navList.classList.toggle("element-none");
+//         console.log("1 - worked!")
+//     })
 
-//         // Loads and shows the element 
-//         if(menuCheckbox.checked) {
-//             // Wait until the transition ends to apply the styles 
-//             navList.addEventListener("transitionend", () => {
-//                 navList.style.display = 'block';
-//                 console.log("1 - worked!")
-//             })
-//         }
-//         // Prevents the element to be loaded by the page
-//         else {
-//             navList.addEventListener("transitionend", () => {
-//                 navList.style.display = 'none';
-//                 console.log("2 - worked!")
-//             })
-//         }
+//         // // Loads and shows the element 
+//         // if(menuCheckbox.checked) {
+//         //     // Wait until the transition ends to apply the styles 
+            
+//         // }
+//         // // Prevents the element to be loaded by the page
+//         // else {
+//         //     navList.addEventListener("transitionend", () => {
+//         //         navList.classList.toggle("element-none");
+//         //         console.log("2 - worked!")
+//         //     })
+//         // }
 // }
+
